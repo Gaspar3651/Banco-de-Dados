10e55176f09356108161c1001c2fb03616b5c3f0
Gaspar3651 <andrielysson.barbosa@gmail.com>
2021-12-09 14:54:56 -0300

    Joguinho de adivinha
    MVC Básico do projeto

---
+++ .NET/Adivinha/Adivinha/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Adivinha.Startup))]
+namespace Adivinha
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
